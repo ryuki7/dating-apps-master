bd2c83df776af565b4c36845f72338123afe2710
ryuki7 <ryuki111209@i.softbank.jp>
2021-08-31 01:11:28 +0900

    [Fix]各アプリ攻略(説明)機能修正

--- app/controllers/app_strategies_controller.rb
+++ app/controllers/app_strategies_controller.rb
@@ -5,20 +5,7 @@ class AppStrategiesController < ApplicationController
     @apps = App.all
   end
 
-  def show
-    app_strategies_page_transition_count = session[:app_strategies_page_transition_count]
-    if app_strategies_page_transition_count
-      session[:app_strategies_page_transition_count] = session[:app_strategies_page_transition_count] + 1
-    else
-      session[:app_strategies_page_transition_count] = 1
-    end
-  
-    if app_strategies_page_transition_count == 3
-      session[:app_strategies_page_transition_count] = nil
-      app_strategies_page_transition_count = nil
-      redirect_to load_app_strategy_path(params[:id])
-    end
-  end
+  def show; end
 
   def make_girlfriend; end
 
